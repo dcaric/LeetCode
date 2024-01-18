5bc46cc8cc6efc7db52c2591e9e1199e91d5c9e5
dcaric <dcaric@me.com>
2024-01-18 09:36:51 +0100

    Added c++ Majoriy Element

---
+++ c++/MajorityElementCpp/MajoritzElementCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MajoritzElementCpp
+//
+//  Created by Dario Caric on 18.01.2024..
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
